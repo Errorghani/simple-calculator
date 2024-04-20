438bcebe8019c4cee9bec5726262ec3a53e6a1de
Errorghani <ab.ghani333221@gmail.com>
2024-04-20 15:28:56 +0500

    feat :  build calculator project

---
+++ index.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "enter first number", type: "number", name: "firstNumber" },
+  {message: "enter secont number", type: "number", name: "secondNumber"},
+  {message: "slect one of the opretors to perform action",
+type: "list",
+name: "operator",
+choices: ["addition", "subtraction","multiplication","division"]
+},
+]);
+if(answer.operator === "addition"){
+  console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "subtraction"){console.log(answer.firstNumber - answer.secondNumber);}
+else if (answer.operator === "multiplication"){console.log(answer.firstNumber * answer.secondNumber);}
+else if (answer.operator === "division"){console.log(answer.firstNumber / answer.secondNumber);}
+else { console.log( "please slect valid operator" ) 
+}
+
